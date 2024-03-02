c03e4be1e044c97d034da985a6d97192ff1e0508
muhai <muhai.li@tuya.com>
2024-03-02 16:53:47 +0800

    chore: update

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,38 @@
-import { Avatar } from 'antd'
+import { Avatar, Dropdown, Menu, Modal, Popover, message } from 'antd'
 import React from 'react'
 
 import { useAppSelector } from '@/store/store'
 import { formatUserName } from '@/utils/formatUserName'
 
 import { Contain, LeftContent, RightContent } from './index.style'
+import { clearToken } from '@/api'
+import { useNavigate } from 'react-router'
 
 export interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = ({}) => {
   const userInfo = useAppSelector((state) => state?.common?.userInfo)
+  const navigate = useNavigate()
+
+  const menu = (
+    <Menu>
+      <Menu.Item
+        onClick={() =>
+          Modal.confirm({
+            title: 'Sign Out',
+            content: 'Continue?',
+            onOk() {
+              clearToken()
+              navigate('/login')
+              message.success('Sign out.')
+            },
+          })
+        }
+      >
+        Sign Out
+      </Menu.Item>
+    </Menu>
+  )
 
   return (
     <Contain>
@@ -17,7 +40,9 @@ export const Header: React.FC<HeaderProps> = ({}) => {
         <div>Todolist TreeView</div>
       </LeftContent>
       <RightContent>
-        <Avatar size="small">{formatUserName(userInfo?.username)}</Avatar>
+        <Dropdown trigger={['hover']} overlay={menu}>
+          <Avatar size="small">{formatUserName(userInfo?.username)}</Avatar>
+        </Dropdown>
       </RightContent>
     </Contain>
   )
